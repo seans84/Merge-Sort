8278357d053d73c8658ee42a0f05a9af43de2ec6
Sean Szurko <sean_szurko@yahoo.com>
2015-09-08 03:57:35 -0700

    Finish recurssion

--- fib.rb
+++ fib.rb
@@ -2,17 +2,22 @@ def fibs(n)
   first = 0
   second = 1
   fibonacci_array = []
-  n.times do |i|
+  (0..n).each do |i|
     fibonacci_array[i] = first
     first, second = second, first+second
   end
   fibonacci_array
 end
   
-print fibs(2)
+print fibs(8)
+puts 
 
 def fibs_rec(n)
-  
+  return [0] if n == 0
+  return [0, 1] if n == 1
+  arr = fibs_rec (n-1)
+  arr << arr[-1]+arr[-2]
 end
 
-print fibs_rec(3)
+print fibs_rec(8)
+puts
